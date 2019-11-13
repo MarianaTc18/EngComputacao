e51616886b6b4fb97698b5e95665981e29a27dad
MarianaTc18 <35478207+MarianaTc18@users.noreply.github.com>
2019-11-13 12:49:43 -0300

    Add files via upload

--- scriptfinal.py
+++ scriptfinal.py
@@ -3,39 +3,31 @@ import os
 import time
 from datetime import datetime
 
-#função que calcula o número de linhas em um arquivo
-def nLinhas (nomeArquivo):
-    arquivo = open(nomeArquivo, "r");
-    linhaUnica = "";
-    for linha in arquivo:
-        linhaUnica += linha + "#";
-    arquivo.close();
-    linhas = linhaUnica.split("#");
-    nlinhas = len(linhas) - 1;
-    return nlinhas;
+#função que adiciona data e hora
+def formatoCsv(valor):
+    valor1 = str(valor.replace("b'", "").replace("\\r\\n'", ""))  # retira excessos
+    valor2 = valor1.strip()
+    now = datetime.now()  # captura data e hora
+    data = str(now.day) + "/" + str(now.month) + "/" + str(now.year)
+    hora = str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
+    # adicionada informações a dados completos no formato 0,5.2,13/11/2019,08:43:48
+    formato = valor2 + "," + data + "," + hora + "\n"
+    return formato
+
+#criaplanilha
+with open("planilha.csv", "w") as dados:
+    escreve2 = dados.write("Identificador,Valor Capturado, Data, Hora\n")
 
 #loop que recebe as informações e adiciona data e hora
 cont = 0
-while (cont < 5):
-       with open("dados.txt","r") as dados, open("dadoscomp.txt","a") as dadosc:
-            limit = nLinhas("dados.txt")
-            for i in range(0,limit):
-                lertxt = dados.readline() #ler os dados no formato b'0,5.2\r\n'
-                valor = str(lertxt.replace("b'","").replace("\\r\\n'", "")) #retira excessos
-                valor2 = valor.strip()
-                now = datetime.now() #captura data e hora
-                data = str(now.day) + "/" + str(now.month) + "/" + str(now.year)
-                hora = str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
-                #adicionada informações a dados completos no formato 0,5.2,13/11/2019,08:43:48
-                escreve = dadosc.write(valor2 + "," + data + "," + hora + "\n")
+while (cont < 3):
+       with open("dados5.txt","r") as dados, open("planilha.csv","a") as planilha:
+                lines = dados.readlines() #ler os dados no formato b'0,5.2\r\n'
+                planilha.write(formatoCsv(lines[cont]))
                 time.sleep(1)
-                cont=cont + 1
-
-#transforma o arquivo em csv
-os.rename("dadoscomp.txt","dadoscomp.csv")
-
-#passa os dados coletados para a planilha csv
-with open("dadoscomp.csv","r") as dados:
-        lercsv = pd.read_csv(dados, delimiter=',', quotechar = "\t", names=["Identificador","Valor Capturado", "Data", "Hora"])
-        print(lercsv)
-        lercsv.to_csv(r"planilha.csv", index = False, sep=',', quotechar = "\t")
+       with open("planilha.csv", "r") as planilha:
+            lercsv = pd.read_csv(planilha)
+            print(lercsv)
+       with open("dados5.txt", "a") as dados: # comando provisório
+           escreve = dados.write("\nb'0,47.3\\r\\n'")
+       cont = cont + 1
